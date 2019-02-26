2f087beb52270c4e02639e90a88465d405147b8c
ls-2018 <1214972346@qq.com>
2019-02-26 14:19:10 +0800

    _

--- Django企业开发实战/comment/templatetags/comment_block.py
+++ Django企业开发实战/comment/templatetags/comment_block.py
@@ -13,3 +13,12 @@ def comment_block(target):
         'comment_form': CommentForm(),
         'comment_list': Comment.get_by_target(target)
     }
+
+
+"""
+因为是自定义的标签，因此需要在模板的最上面（但是需要在extends下面）增加{% load comment_block %}
+用来加载我们自定义的标签文件
+
+然后在需要的展示评论的地方增加{% comment_block request.path %} 
+这里的request全局变量
+"""
