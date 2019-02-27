540808cc73f82b3d88e2788aab224a614928612d
ls-2018 <1214972346@qq.com>
2019-02-27 13:44:22 +0800

    django-ckeditor

--- Django企业开发实战/blog/xadmin_utils/adminforms.py
+++ Django企业开发实战/blog/xadmin_utils/adminforms.py
@@ -1,6 +1,7 @@
 from django import forms
 from dal import autocomplete
 from blog.models import Category, Tag, Post
+from ckeditor.widgets import CKEditorUploadingWidget
 
 
 class PostAdminForm(forms.ModelForm):
@@ -16,7 +17,35 @@ class PostAdminForm(forms.ModelForm):
         widget=autocomplete.ModelSelect2(url='tag-autocomplete'),
         label='标签',
     )
+    content_html = forms.CharField(widget=CKEditorUploadingWidget(), label='正文', required=False)
+    content = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     class Meta:
         model = Post
-        fields = ('category', 'tag', 'title', 'desc', 'content', 'status')
+        fields = ('category', 'tag', 'title', 'desc', 'content', 'status', 'is_md')
+
+    class Media:
+        js = ('js/post_editor.js',)
+
+    def __init__(self, instance=None, initial=None, **kwargs):
+        initial = initial or {}
+        if instance:
+            if instance.is_md:
+                initial['content_md'] = instance.content
+            else:
+                initial['content_ck'] = instance.content
+
+        super().__init__(instance=instance, initial=initial, **kwargs)
+
+    def clean(self):
+        is_md = self.cleaned_data.get('is_md')
+        if is_md:
+            content_field_name = 'content_md'
+        else:
+            content_field_name = 'content_ck'
+        content = self.cleaned_data.get(content_field_name)
+        if not content:
+            self.add_error(content_field_name, '必填项！')
+            return
+        self.cleaned_data['content'] = content
+        return super().clean()
