422a34a1e824ec1734e99e2715505ec123ed0f2c
ls-2018 <1214972346@qq.com>
2019-02-27 08:12:42 +0800

    完整版o(╥﹏╥)o

--- Django企业开发实战/Django企业开发实战/custom_site.py
+++ Django企业开发实战/Django企业开发实战/custom_site.py
@@ -2,8 +2,8 @@ from django.contrib.admin import AdminSite
 
 
 class CustomSite(AdminSite):
-    site_header = 'Typeidea'
-    site_title = 'Typeidea管理后台'
+    site_header = 'Django'
+    site_title = 'Django管理后台'
     index_title = '首页'
 
 
