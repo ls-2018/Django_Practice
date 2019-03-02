a0150099e38f6f1cedd2d070b7b29c47b95d7626
ls-2018 <1214972346@qq.com>
2019-03-02 16:32:10 +0800

    python从菜鸟到高手

---
+++ others/python从菜鸟到高手/第八章/类.py
@@ -0,0 +1,30 @@
+class Student:
+    def __run(self):
+        print("run")
+
+    def play(self):
+        pass
+
+
+s = Student()
+print(Student.__dict__)
+print(s.__class__.__dict__)
+print(s.__dict__)
+
+s._Student__run()
+
+# 获去类中所有的方法
+import inspect
+
+methods = inspect.getmembers(s, predicate=inspect.ismethod)
+for i in methods:
+    print(i)
+
+from inspect import isfunction, ismethod
+
+print(isfunction(s.play))
+print(ismethod(s.play))
+from types import FunctionType, MethodType
+
+print(isinstance(s.play, FunctionType))
+print(isinstance(s.play, MethodType))
