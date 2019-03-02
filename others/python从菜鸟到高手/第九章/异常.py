666bdb3549fcf9a09274152f8b17aca5181fa967
ls-2018 <1214972346@qq.com>
2019-03-02 16:43:27 +0800

    python从菜鸟到高手

---
+++ others/python从菜鸟到高手/第九章/异常.py
@@ -0,0 +1,12 @@
+"""
+Exception               所有异常的基类
+AttributeError          属性引用或赋值失败
+OSError                 当前系统无法执行任务
+IndexError
+KeyError                在使用映射中不存在的键值时
+NameError               找不到变量
+SyntaxError             语法错误
+TypeError
+ValueError
+ZeroDivisionError       除除法或者取模操作的第二个参数值为0时抛出的异常
+"""
