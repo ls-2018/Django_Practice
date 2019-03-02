3391a6fa384758909c70a94ad6735ff1c7d897af
ls-2018 <1214972346@qq.com>
2019-03-02 14:24:13 +0800

    python从菜鸟到高手

---
+++ others/python从菜鸟到高手/第二章/基本语法.py
@@ -0,0 +1,29 @@
+# format
+x = 123456.789
+print(format(x, '_>20.5f'))
+print(format(x, '_^20.5f'))
+print(format(x, '0.2E'))
+print(format(x, '0.2e'))
+
+# 数值
+print(bin(1000))
+print(oct(1000))
+print(bin(1000))
+print(hex(1000))
+
+# python解释器無法判斷字符串中間的引號是單引號還是正常的字符串
+print(repr('hello \n world'))  # repr按原始字符串輸出， 會用一對單引號括起來
+# 转义的三种
+"""
+repr
+转义符号\
+在字符前面加r
+"""
+
+print("hello\n"
+      "world" ,end='\n',sep=',')  # 输出一行
+
+
+
+# 为空的值
+# False 0 [] {}  '' 
