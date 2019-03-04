c625ddbf4e6448726bae511799af82d40a42a2d2
ls-2018 <1214972346@qq.com>
2019-03-04 21:25:35 +0800

    Python语言及其应用

---
+++ others/Python语言及其应用/WSGI服务器.py
@@ -0,0 +1,18 @@
+'''
+使用多进程或线程来处理并发请求
+
+
+uwsgi               http://projects.unbit.it/uwsgi/
+cherrypy            http://www.cherrypy.org
+pulons              http://www.pylonsproject.org
+
+
+
+
+# 基于事件的服务器,只使用单线程但不会阻塞
+tornado             http://www.tornadoweb.org
+gevent              http://gevent.org
+gunicorn            http://gunicorn.org
+
+
+'''
