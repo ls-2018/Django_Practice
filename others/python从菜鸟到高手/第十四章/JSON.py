9d483efc6489e7e4673200bf96c235c10307a2b5
ls-2018 <1214972346@qq.com>
2019-03-04 09:27:36 +0800

    python从菜鸟到高手

--- others/python从菜鸟到高手/第十四章/JSON.py
+++ others/python从菜鸟到高手/第十四章/JSON.py
@@ -21,3 +21,29 @@ my1 = json.loads(demo,object_hook=json2Product)
 print(my1)
 print(my1.age)
 print(my1.name)
+
+
+
+
+
+
+
+
+
+
+
+import json
+class Product:
+    def __init__(self, name,price,count):
+        self.name = name
+        self.price = price
+        self.count = count
+def product2Dict(obj):
+    return {
+        'name': obj.name,
+        'price': obj.price,
+        'count': obj.count
+    }
+product = Product('特斯拉',1000000,20)
+jsonStr = json.dumps(product, default=product2Dict,ensure_ascii=False)
+print(jsonStr)
